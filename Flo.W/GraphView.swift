6a0b8e3efba4cefae9eb982afd91fcb53bdbf027
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2018-04-28 22:19:25 +0800

    Part 2 - Initialized

---
+++ Flo.W/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Flo.W
+//
+//  Created by Francis Jemuel Bergonia on 28/04/2018.
+//  Copyright © 2018 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
